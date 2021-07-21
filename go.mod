59bcd7aad8686fab5c1f7de4c56a98d15ad16bdb
= <toolnado@gmail.com>
2021-07-21 18:14:37 +0300

    Added jwt token authentication

--- go.mod
+++ go.mod
@@ -3,12 +3,14 @@ module github.com/toolnado/rest-todo-app
 go 1.16
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.7.2
 	github.com/go-playground/validator/v10 v10.7.0 // indirect
 	github.com/jmoiron/sqlx v1.3.4
 	github.com/joho/godotenv v1.3.0
+	github.com/kr/pretty v0.2.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/lib/pq v1.2.0
+	github.com/lib/pq v1.10.2
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/sirupsen/logrus v1.8.1
@@ -18,4 +20,5 @@ require (
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -3,15 +3,27 @@ package service
 import (
 	"crypto/sha1"
 	"fmt"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 
 	todo "github.com/toolnado/rest-todo-app"
 	"github.com/toolnado/rest-todo-app/pkg/repository"
 )
 
-const salt = "wefjefkek24234235kqklwshmnmkjk"
+const (
+	salt       = "wefjefkek24234235kqklwshmnmkjk"
+	signingKey = "wefaef#$%1q2431543643rhGGESC24234235kqklw"
+	tockenTTl  = 12 * time.Hour
+)
 
 type AuthService struct {
 	repo repository.Authorization
+}
+
+type tokenKlaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
 }
 
 func NewAuthService(repo repository.Authorization) *AuthService {
@@ -21,6 +33,23 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenKlaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tockenTTl).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func generatePasswordHash(password string) string {
